d812afc2c1d4a3802815f4d9a10d7654e7ffbb56
Alejandro Hernández Hernández <alejhern.student@42barcelona.com>
2026-07-07 16:02:21 +0200

    Refactor WordPress setup script to improve database connection handling and enhance readability

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,14 +1,46 @@
-#! /bin/bash
+#!/bin/bash
 
-if [ -f ./wp-config.php ]
-then
-	echo "Wordpress already exists"
-else
-	wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-	wp core install --url=$DONAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS  --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
-	wp theme install twentysixteen --activate --allow-root
+cd /var/www/html
+
+# Esperar a MariaDB
+until mariadb -h"${MYSQL_HOSTNAME}" \
+    -u"${MYSQL_USER}" \
+    -p"${MYSQL_PASSWORD}" \
+    -e "SELECT 1;" >/dev/null 2>&1
+do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+
+# Descargar WordPress solo si no existe
+if [ ! -f wp-config.php ]; then
+
+    wp core download --allow-root
+
+    wp config create \
+        --dbname="${MYSQL_DATABASE}" \
+        --dbuser="${MYSQL_USER}" \
+        --dbpass="${MYSQL_PASSWORD}" \
+        --dbhost="${MYSQL_HOSTNAME}" \
+        --allow-root
+
+    wp core install \
+        --url="https://${DOMAIN_NAME}" \
+        --title="${WORDPRESS_TITLE}" \
+        --admin_user="${WORDPRESS_ADMIN}" \
+        --admin_password="${WORDPRESS_ADMIN_PASS}" \
+        --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+        --skip-email \
+        --allow-root
+
+    wp user create \
+        "${WORDPRESS_USER}" \
+        "${WORDPRESS_EMAIL}" \
+        --role=author \
+        --user_pass="${WORDPRESS_USER_PASS}" \
+        --allow-root
+
+    wp theme install twentysixteen --activate --allow-root
 fi
 
-/usr/sbin/php-fpm8.4 -F;
+exec php-fpm8.4 -F
