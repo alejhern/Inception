c6db8d0093a02143ec01279974cbca1a96118bcc
Alejandro Hernández Herández <alejhern@student.42barcelona.com>
2026-07-08 16:20:35 +0100

    Refactor MariaDB setup script: streamline initialization and user creation process

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
+set -e
 
-# Inicializar la base de datos si no existe
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d /var/lib/mysql/mysql ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# Arrancar MariaDB temporalmente
-mysqld_safe --datadir=/var/lib/mysql &
+mariadbd-safe --datadir=/var/lib/mysql &
 
-# Esperar a que esté disponible
 until mariadb-admin ping --silent; do
     sleep 1
 done
 
-# Crear base de datos y usuarios solo la primera vez
-mariadb -u root << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+if [ ! -f /var/lib/mysql/.initialized ]; then
 
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+    mariadb <<EOF
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+    CREATE DATABASE IF NOT EXISTS \`${UPTIME_KUMA_DATABASE}\`;
 
-FLUSH PRIVILEGES;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+    GRANT ALL PRIVILEGES ON \`${UPTIME_KUMA_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+    FLUSH PRIVILEGES;
 EOF
 
-# Crear base de datos y usuario para uptime-kuma
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" << EOF
-CREATE DATABASE IF NOT EXISTS \`${UPTIME_KUMA_DATABASE}\`;
-GRANT ALL PRIVILEGES ON \`${UPTIME_KUMA_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
+    mariadb -uroot -p"${MYSQL_ROOT_PASSWORD}" "${UPTIME_KUMA_DATABASE}" < /tmp/db.sql
 
-#cargar la base de datos de Uptime Kuma
-mariadb -u root -p"${MYSQL_ROOT_PASSWORD}" "${UPTIME_KUMA_DATABASE}" < /tmp/db.sql
+    touch /var/lib/mysql/.initialized
+fi
 
-# Parar la instancia temporal
 mariadb-admin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-# Arrancar MariaDB en primer plano
 exec mariadbd --user=mysql --console
